f71d84da1394f030e60131cffea6b3489a9374ee
CeSeDu <senad8127@gmail.com>
2023-01-11 00:47:24 +0300

    Büt
    Signed-off-by: CeSeDu <senad8127@gmail.com>

---
+++ enesBayramJSDersleri/finalVizeHesapla.js
@@ -0,0 +1,14 @@
+let vize = Number(prompt("vize notunu giriniz"));
+let final = Number(prompt("final notunu giriniz"));
+
+let ortalama =vize*0.3 + final*0.7;
+
+if(ortalama>=35){
+    alert("dersten geçtiniz" + ortalama);    
+}else{
+    alert("kaldınız" + ortalama);
+}
+
+
+
+
